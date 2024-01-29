b4292db7661c22066f847ebb4f43f610671e8563
kwaiidoesthings <153032591+kwaiidoesthings@users.noreply.github.com>
2024-01-29 12:03:57 -0500

    restaurant bill calculator
    takes the input of a meal and adds tip of 20% and tax of 6.75% and outputs the total and the breakdown of all the additions to the meal payment

---
+++ restaurantbill.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class restaurantbill {
+    public static void main(String[] args) {
+    // const declaring tax and tip rate
+      final double tax = .0675;
+      final double tip = .20; 
+    // vars declaring different factors 
+      double meal;
+      double tax_amount;
+      double tip_amount;
+      double meal_final;
+    // takes input of amount of the meal
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter meal amount: ");
+      meal = input.nextDouble();
+    // math calc for tip tax and final meal bill
+      tax_amount = meal * tax;
+      tip_amount = meal * tip;
+      meal_final = meal + tip_amount + tax_amount;
+    // output (should i always be using formatting (%n %s etc?))
+      System.out.println("Meal: " + meal);
+      System.out.println("Tax: " + tax_amount);
+      System.out.println("Tip: " + tip_amount);
+      System.out.println("Total: " + meal_final);
+    }
+}
