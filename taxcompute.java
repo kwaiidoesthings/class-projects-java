67cfadf7d72757bbcd440648af03160a004c930e
kwaiidoesthings <153032591+kwaiidoesthings@users.noreply.github.com>
2024-01-29 12:04:26 -0500

    tax calculator
    adds a state tax of 4% and a county tax of 2% and outputs it out to a number

---
+++ taxcompute.java
@@ -0,0 +1,33 @@
+//scanner lib to take input
+import java.util.Scanner;
+
+// class header
+
+public class taxcompute {
+
+    public static void main(String[] args) {
+    // variables to take base number
+      double num;
+      double state_num; 
+      double county_num; 
+      double num2;
+    // consts
+      final double state_tax = .04;
+      final double county_tax = .02;
+      
+    // takes input 
+      Scanner input = new Scanner(System.in);
+        System.out.print("Enter Payment: ");
+        num = input.nextDouble();
+
+    // math calc to get final output
+        state_num = num * state_tax;
+        county_num = num * county_tax;
+        num2 = num + county_num + state_num;
+      
+    // outputs
+      System.out.println(num2);
+
+}
+}
+
