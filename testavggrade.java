06ef930ed7288030b49dd3c453282ad447260ffd
kwaiidoesthings <153032591+kwaiidoesthings@users.noreply.github.com>
2024-03-04 11:05:50 -0500

    takes 5 grades and averages them

--- testavggrade.java
+++ testavggrade.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class testavggrade {
 
+static void calcAverage(int[] score){
+
+}
 public static void main(String[] args) {
     int[] score = new int[5];
     Scanner input = new Scanner(System.in);
@@ -9,4 +12,10 @@ public static void main(String[] args) {
     for (int i = 0; i < 5; i++){
         System.out.print("Enter Score " + (i + 1) + ": ");
         score[i] = input.nextInt();
-    }}}
+}
+
+
+
+
+
+input.close();}}
