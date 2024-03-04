801b96b1b117ee147235a0ae41a9f7466d420c04
kwaiidoesthings <153032591+kwaiidoesthings@users.noreply.github.com>
2024-03-04 11:05:37 -0500

    falling distance determine
    returns value 1-10 in 1/2 gh^2

--- fallingDistance1.java
+++ fallingDistance1.java
@@ -8,13 +8,15 @@ public class fallingDistance1 {
  d = 1/2 gt^2 represented by .5 * 9.8(gravity) * Math.pow(time,2), Math.pow puts time to the power of 2
  prints solution after
 */
-static void fallingDistance(int time){
-DecimalFormat numberFormat = new DecimalFormat("#.00");
+public static double fallingDistance(int time){
+
 double solution = .5 * 9.8 * Math.pow(time,2);
-System.out.println(numberFormat.format(solution) + " meters");
+    return solution;
     }
 // for loop that iterates 1-10 and calls fallingDistance to i to simulate test arguments 1-10
 public static void main(String[] args) {
+DecimalFormat numberFormat = new DecimalFormat("#.00");
     for (int i = 1; i < 11; i++){
-        fallingDistance(i);
+        System.out.println(numberFormat.format(fallingDistance(i)) + " meters");
+        
 }}}
